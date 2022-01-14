b34a4841aebb931767e25b3b5ea82298aeb914bc
Drgil666 <2574105038@qq.com>
2022-01-14 17:09:58 +0800

    Fears:新增个人信息类工具类字段。

--- src/main/java/com/project/gimme/utils/PersonalMsgUtil.java
+++ src/main/java/com/project/gimme/utils/PersonalMsgUtil.java
@@ -23,6 +23,9 @@ public class PersonalMsgUtil {
     private static final String INSERT_CHANNEL_MEMBER_ATTRIBUTE = "insert_channel_member";
     private static final String UPDATE_CHANNEL_MEMBER_ATTRIBUTE = "update_channel_member";
     private static final String DELETE_CHANNEL_MEMBER_ATTRIBUTE = "delete_channel_member";
+    private static final String ACCEPT_ATTRIBUTE = "accept";
+    private static final String REJECT_ATTRIBUTE = "reject";
+    private static final String NULL_ATTRIBUTE = "null";
 
     @AllArgsConstructor
     @Getter
@@ -83,8 +86,37 @@ public class PersonalMsgUtil {
         private final String name;
     }
 
+    @AllArgsConstructor
+    @Getter
+    public enum Status {
+        /**
+         * 请求已同意
+         */
+        TYPE_ACCEPT(0, ACCEPT_ATTRIBUTE),
+        /**
+         * 请求已拒绝
+         */
+        TYPE_REJECT(1, REJECT_ATTRIBUTE),
+        /**
+         * 空信息
+         */
+        TYPE_NULL(2, NULL_ATTRIBUTE);
+        private final Integer code;
+        private final String name;
+    }
+
     public static final Character[] CHARACTER_LIST = Character.values();
     public static final HashMap<String, Integer> CHARACTER_MAP = getCharacterMap();
+    public static final Status[] STATUS_LIST = Status.values();
+    public static final HashMap<String, Integer> STATUS_MAP = getStatusMap();
+
+    public static HashMap<String, Integer> getStatusMap() {
+        HashMap<String, Integer> hashMap = new HashMap<>(10);
+        for (Status status : STATUS_LIST) {
+            hashMap.put(status.name, status.code);
+        }
+        return hashMap;
+    }
 
     public static HashMap<String, Integer> getCharacterMap() {
         HashMap<String, Integer> hashMap = new HashMap<>(10);
@@ -97,6 +129,13 @@ public class PersonalMsgUtil {
     public static Integer getCharacterByName(String name) {
         if (CHARACTER_MAP.containsKey(name)) {
             return CHARACTER_MAP.get(name);
+        }
+        return null;
+    }
+
+    public static Integer getStatusByName(String name) {
+        if (STATUS_MAP.containsKey(name)) {
+            return STATUS_MAP.get(name);
         }
         return null;
     }
