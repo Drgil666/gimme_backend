9ec8cc4b9dc67d1b5bdd9ad737917ec35b41dfd4
Drgil666 <2574105038@qq.com>
2022-01-19 15:54:50 +0800

    Fears:新增用户信息中间类VO。

---
+++ src/main/java/com/project/gimme/pojo/vo/UserVO.java
@@ -0,0 +1,20 @@
+package com.project.gimme.pojo.vo;
+
+import com.project.gimme.pojo.User;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author DrGilbert
+ * @date 2022/1/19 15:53
+ */
+@Data
+@ApiModel(description = "用户信息中间类")
+public class UserVO extends User {
+    /**
+     * 好友备注
+     */
+    @ApiModelProperty(value = "好友备注", name = "note")
+    private String note;
+}
